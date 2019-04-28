f5150d818d466881be91fb9ee3a786f12f414928
panyongxu <panyx_1002@163.com>
2019-04-28 18:33:44 +0800

    静态页面初步完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -178,7 +178,43 @@
           </div>
         </div>
       </div>
-      <div class="right"></div>
+      <div class="right">
+        <div class="search">
+          <div class="right-text-header">
+            春季旅游
+            <div>2019-04-12   阅读 500</div>
+          </div>
+          <div class="right-text right-mb">
+            春游，古称 <span class="blue">踏青</span> ，是一种古老的传统族 <span class="blue">民俗</span> 文体活动，古时一般在上巳节，清明节。“三月三日气象新，长安水边多丽人。”杜甫描绘的
+          就是唐代人们春游的盛况。 春季郊野，万木吐翠，芳草茵茵，百鸟争鸣，阳光和熙，空气清新，置身于这如诗如画的环境中，能使人心胸
+          开阔，疲劳消除，精神振奋，还能促进细胞的新陈代谢，改善血液循环，增加腰腿肌肉的活动，加强心脏和肺的功能，可降低血脂、血压、
+          防治心血管病，因而，春游具有特殊的保健作用.芳草茵茵，百鸟争明，阳光和熙，空气清新，置身于这如诗如画的环境中，能使人心胸
+          开阔，疲劳消除，精神振奋，还能促进细胞的新陈代谢，改善血液循环，增加腰腿肌肉的活动，加强心脏和肺的功能，可降低血脂、血压、
+          防治心血管病，因而，春游具有特殊的保健作用.芳草茵茵，<span class="red">百鸟争鸣</span>，阳光和熙，空气清新，置身于这如诗如画的环境中，能使人心胸
+          开阔，疲劳消除，精神振奋，还能促进细胞的新陈代谢，改善血液循环，增加腰腿肌肉的活动，加强心脏和肺的功能，可降低血脂、血压、
+          防治心血管病，因而，春游具有特殊的保健作用.芳草茵茵，百鸟争鸣，阳光和熙，空气清新，置身于这如诗如画的环境中，能使人心胸
+          开阔，疲劳消除，精神振奋.
+          </div>
+          <div class="right-mb ">
+            <div class="pic1"></div>
+          </div>
+          <div class="right-mb ">
+            <div class="text">
+              <p>知人者智，自知者明。胜人者有力，自胜者强。</p>
+              <p>老子,发表于2019年人民日报引用说明</p>
+            </div>
+          </div>
+          <div class="right-mb video1">
+            <video controls="controls" src="./../assets/video.mp4"></video>
+          </div>
+          <div class="right-mb">
+            <div class="pic2"></div>
+          </div>
+          <div class="footer">
+             <el-button type="primary">返回</el-button>
+          </div>
+        </div>
+      </div>
 
     </main>
 
@@ -297,6 +333,10 @@ export default {
   color: red;
   text-decoration: underline;
 }
+ .red {
+  color: red;
+  text-decoration: underline;
+}
 .search.toal i {
   margin-right: 30px;
   font-size: 25px;
@@ -371,6 +411,8 @@ main .right {
   background-color: #ffffff;
   border-radius: 6px;
   min-height: 1000px;
+   padding: 30px;
+   box-sizing: border-box;
 }
 .tool {
   width: 60px;
@@ -378,5 +420,63 @@ main .right {
   position: absolute;
   top: 50px;
   left: -60px;
+}
+.search .right-text-header {
+  text-align: left;
+  font-size: 20px;
+  padding: 20px;
+}
+.search .right-text-header  div {
+  font-size: 14px;
+	font-weight: normal;
+	font-stretch: normal;
+	line-height: 27px;
+	letter-spacing: 0px;
+	color: #999999
+}
+.search .right-text {
+  font-size: 16px;
+	line-height: 30px;
+	color: #333333;
+  text-align: left;
+  text-indent: 2em; 
+  padding: 20px;
+}
+.right-mb{
+  margin-bottom: 20px;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.right-mb .pic1{
+  height: 280px;
+  background: url("./../assets/p1.jpg") no-repeat;
+  background-size: cover;
+}
+.right-mb .pic2{
+  height: 280px;
+  background: url("./../assets/p2.jpg") no-repeat;
+  background-size: cover;
+}
+.right-mb .text{
+  border-left: 5px solid #000;
+  text-align: left;
+  padding-left: 20px;
+}
+.right-mb .text p:nth-child(1){
+  font-size: 18px;
+	line-height: 27px;
+	color: #000000;
+}
+.right-mb .text p:nth-child(2){
+  font-size: 14px;
+	line-height: 27px;
+	color: #4f4f4f;
+}
+.right-mb.video1{
+  height: 280px;
+}
+.footer {
+  margin: 40px 0;
 }
 </style>
